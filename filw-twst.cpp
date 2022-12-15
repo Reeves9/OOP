4743db783d6a8e099527b34e72eec4fe392d98a9
Reeves9 <reeshav.mainali1990@gmail.com>
2022-12-15 08:55:54 +0545

    file handling

--- filw-twst.cpp
+++ filw-twst.cpp
@@ -16,10 +16,10 @@ int main()
     ofstream file("record.txt");
     for (int i=0;i<4;i++)
     {
-        cout << "for Employee " << i << endl;
+        cout << "for Employee " << i+1 << endl;
         cout << "enter name: " << endl;
         cin >> record[i].name;
-        record[i].id = i;
+        record[i].id = i+1;
         cout << "enter address: " << endl;
         cin >> record[i].address;
         cout << "enter department: " << endl;
@@ -27,11 +27,12 @@ int main()
         cout << "enter salary: " <<endl;
         cin >> record[i].salary;
 
-        file << "id:         " <<record[i].id<<endl;
-        file << "name:       " <<record[i].name<<endl;
-        file << "department: " <<record[i].department<<endl;
-        file << "address:    " <<record[i].address<<endl;
-        file << "salary:     " <<record[i].salary<<endl;
+        file << "id         :" <<record[i].id<<endl;
+        file << "name       :" <<record[i].name<<endl;
+        file << "department :" <<record[i].department<<endl;
+        file << "address    :" <<record[i].address<<endl;
+        file << "salary     :" <<record[i].salary<<endl;
+        file << endl;
     }
     file.close();
     return 0;
