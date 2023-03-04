e040cde0474f49095d565f4e10f6351709dca118
Reeshav Mainali <reeshav.mainali1990@gmail.com>
2023-03-04 16:12:22 +0545

    fooling around

--- encrypt.cpp
+++ encrypt.cpp
@@ -19,15 +19,62 @@ string encrypt(string plaintext, int shift)
     return cyphertext;
 }
 
+string decrypt(string ciphertext, int shift)
+{
+    string plaintext = "";
+    for (int i = 0; i < ciphertext.length(); i++)
+    {
+        char c = ciphertext[i];
+        if (isalpha(c))
+        {
+            c = toupper(c);
+            c = (c - 'A' - shift + 26) % 26 + 'A';
+        }
+        plaintext += c;
+    }
+    return plaintext;
+}
+
 int main()
 {
-    string plaintext;
-    int shift;
-    cout << "Enter any word" << endl;
-    cin >> plaintext;
-    cout << "Enter key" << endl;
-    cin >> shift;
-    string cyphertext = encrypt(plaintext, shift);
-    cout << "encrypted string is: " << cyphertext << endl;
+    string plaintext, cyphertext;
+    int shift, choice;
+start:
+    system("clear");
+    cout << "1.Encrypt" << endl;
+    cout << "2.Decrypt" << endl;
+    cout << "Enter your choice" << endl;
+    cin >> choice;
+    if (choice == 1)
+    {
+        system("clear");
+        cout << "Enter Text" << endl;
+        cin >> plaintext;
+        cout << "Enter key:" << endl;
+        cin >> shift;
+        cyphertext = encrypt(plaintext, shift);
+        cout << "Encrypted Text :" << endl;
+        cout << cyphertext << endl;
+        system("read");
+        goto start;
+    }
+    else if (choice == 2)
+    {
+        system("clear");
+        cout << "Enter Cyphertext:" << endl;
+        cin >> cyphertext;
+        cout << "Enter key:" << endl;
+        cin >> shift;
+        plaintext = decrypt(cyphertext, shift);
+        cout << "Plaintext is :" << endl;
+        cout << plaintext << endl;
+        system("read");
+        goto start;
+    }
+    else
+    {
+        cout << "wrong input" << endl;
+        goto start;
+    }
     return 0;
 }
