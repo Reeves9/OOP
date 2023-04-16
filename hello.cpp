6b8b35c23c06b41c49e9f2ece09fa2ac2cac4f5b
Reeshav Mainali <reeshav.mainali1990@gmail.com>
2023-04-16 22:15:51 +0545

    Test

---
+++ hello.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Swapper{
+public:
+	int x,y,t;
+	
+	void Swapper(int x, int y){
+		this->x = x;
+		this->y = y;
+	}
+	
+	void swap(){
+		t = x;
+		x = y;
+		y = t;
+		cout << x << " " << y << endl; 
+	}
+};
+
+int main()
+{
+	int num1 , num2;
+	cout << "emter two numbers"<<endl;
+	cin >> num1 >> num2;
+	Swapper obj(num1,num2);
+	obj.swap();
+	return 0;
+}
