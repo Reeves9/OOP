3691c8280b9324abc8c7e8f663b85ed12ddf3f48
Reeshav Mainali <reeshav.mainali1990@gmail.com>
2023-04-20 13:11:18 +0545

    test

--- ambiguity.cpp
+++ ambiguity.cpp
@@ -4,44 +4,44 @@ using namespace std;
 
 class Grand
 {
-    protected:
+protected:
     int n;
-    
-    public:
+
+public:
     void getter();
     void display();
 };
 
-void Grand :: getter()
+void Grand ::getter()
 {
     cin >> n;
 }
 
-void Grand :: display()
+void Grand ::display()
 {
     cout << "Given value is " << n << endl;
 }
 
-class Parent : public Grand 
+class Parent : public Grand
 {
-    public:
+public:
     void display();
 };
 
-void Parent :: display()
+void Parent ::display()
 {
-    cout << "Square is " << n*n << endl; 
+    cout << "Square is " << n * n << endl;
 }
 
 class Child : public Parent
 {
-    public:
+public:
     void display();
 };
 
 void Child::display()
 {
-    cout << "Cube is " << n*n*n << endl;
+    cout << "Cube is " << n * n * n << endl;
 }
 
 int main()
@@ -53,4 +53,3 @@ int main()
     child.Child::display();
     return 0;
 }
-
